cb5fa53cf119ea17620d820a7c860d7adb1c76e8
Daniel Clarke <d.clarke@queensu.ca>
2018-09-07 10:57:42 -0400

    Adding "fire in the hole" sound effect to blast site

--- Assets/Scripts/PlungerBehaviour.cs
+++ Assets/Scripts/PlungerBehaviour.cs
@@ -14,6 +14,8 @@ public class PlungerBehaviour : MonoBehaviour {
 	public GameObject explosion, box;
 	public VideoPlayer video1;
 
+    public AudioSource fireInTheHole;
+
 	void Update() {
 		if (transform.localPosition.y < 11.495) {
 			transform.localPosition = new Vector3(transform.localPosition.x, 11.495f, transform.localPosition.z);
@@ -79,6 +81,7 @@ public class PlungerBehaviour : MonoBehaviour {
 	//}
 
 	IEnumerator Explosion() {
+        fireInTheHole.Play();
 		video1.Play();
 		yield return new WaitForSeconds(0.5f);
 		
