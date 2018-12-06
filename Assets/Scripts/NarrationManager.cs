930948b0cb0ec3f64b7a8427a358f969080c8c0b
Daniel Clarke <d.clarke@queensu.ca>
2018-12-06 12:45:33 -0500

    Adding Narration to mine and Hub

---
+++ Assets/Scripts/NarrationManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class NarrationManager : MonoBehaviour
+{
+    public static NarrationManager instance;
+    public AudioClip[] narrationClips;
+
+    AudioSource player;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        player = GetComponent<AudioSource>();
+        player.playOnAwake = false;
+    }
+
+    public void PlayClip(int clipIndex)
+    {
+        if(clipIndex < narrationClips.Length && clipIndex >= 0)
+        {
+            player.clip = narrationClips[clipIndex];
+            player.Play();
+        }
+    }
+
+    public void PlayClip(int clipIndex, float delay)
+    {
+        if (clipIndex < narrationClips.Length && clipIndex >= 0)
+        {
+            StartCoroutine(ClipDelay(narrationClips[clipIndex], delay));
+        }
+    }
+
+    IEnumerator ClipDelay(AudioClip clip, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        player.clip = clip;
+    }
+}
